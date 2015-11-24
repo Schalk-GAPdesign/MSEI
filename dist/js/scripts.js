0df8e61155341bb03ce2820d4e06b9b3f219a52e
Schalk <schalk@gapdesign.co.za>
2015-11-24 13:15:11 +0200

    Article Pages (WIP)

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,21 +1,37 @@
-jQuery('.M__mainNavsearch-container').click(function() {
-    jQuery(this).addClass('is-activated')
-    jQuery('#__menuSearch-main').addClass('is-activated')
-    jQuery('#__secondaryNav-main').removeClass('is-activated')
-    jQuery('#__menuMagazine-main').removeClass('is-activated')
-    jQuery('.O__mainNavList-item--magazine').find('.M__mainNavButton-main').removeClass('is-activated')
+jQuery('.M__mainNavsearch-container').click(function () {
+    jQuery('.is-activated').removeClass('is-activated');
+    jQuery(this).addClass('is-activated');
+    jQuery('#__menuSearch-main').addClass('is-activated');
+
 });
 
 
-jQuery('.O__mainNavList-item--magazine').click(function() {
-    jQuery(this).find('.M__mainNavButton-main').addClass('is-activated')
-    jQuery('#__menuMagazine-main').addClass('is-activated')
-    jQuery('#__secondaryNav-main').removeClass('is-activated')
-    jQuery('#__menuSearch-main').removeClass('is-activated')
-    jQuery('.M__mainNavsearch-container').removeClass('is-activated')
-    
-    
-   
+jQuery('.O__mainNavList-item--magazine').click(function () {
+    jQuery('.is-activated').removeClass('is-activated');
+    jQuery(this).find('.M__mainNavButton-main').addClass('is-activated');
+    jQuery('#__menuMagazine-main').addClass('is-activated');
 });
-jQuery('.O__mainNavList-item--magazine').find('.M__mainNavButton-main')
-//# sourceMappingURL=scripts.js.map
+
+
+jQuery('.O__mainNavList-item--magazine').find('.M__mainNavButton-main');
+
+
+$(document).ready(function () {
+    $(".fancybox").fancybox({
+        openEffect: 'fade',
+        /* none, fade, elastic */
+        closeEffect: 'fade',
+        openSpeed: 0,
+        /* ms, "slow", "normal", "fast"*/
+        closeSpeed: 0,
+        /* mouseWheel : false,*/
+        helpers: {
+            /*title : null */
+            title: {
+                type: 'inside' /* 'float', 'inside', 'outside', 'over', 'null' */
+            }
+        }
+    });
+});
+
+
