d2df489647df5312916f67e95549d95ded2953a0
Schalk <schalk@gapdesign.co.za>
2015-11-26 11:49:38 +0200

    Article Template (WIP)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var spritesmith = require('gulp.spritesmith');
- 
+
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('src/_global/utilities/spritesheet/img/**/*.png').pipe(spritesmith({
+  var spriteData = gulp.src('src/_global/spritesheetGenerator/source/**/*.png').pipe(spritesmith({
     imgName: '../img/spritesheet.png',
-    cssName: 'sprite.css'
+    cssName: '../sprite.css'
   }));
-  return spriteData.pipe(gulp.dest('src/_global/utilities/spritesheet'));
+  return spriteData.pipe(gulp.dest('src/_global/spritesheetGenerator/source'));
 });
