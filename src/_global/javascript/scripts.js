97ee84386fe96316dbd75d88bdc4a330d6cd33cf
Schalk <schalk@gapdesign.co.za>
2015-11-29 18:59:51 +0200

    WIP

--- src/_global/javascript/scripts.js
+++ src/_global/javascript/scripts.js
@@ -1,36 +1,37 @@
-jQuery('.M__mainNavsearch-container').click(function () {
-    jQuery('.is-activated').removeClass('is-activated');
-    jQuery(this).addClass('is-activated');
-    jQuery('#__menuSearch-main').addClass('is-activated');
+jQuery(document).ready(function () {
 
-});
+    jQuery('.M__mainNavsearch-container').click(function () {
+        jQuery('.is-activated').removeClass('is-activated');
+        jQuery(this).addClass('is-activated');
+        jQuery('#__menuSearch-main').addClass('is-activated');
 
+    });
 
-jQuery('.O__mainNavList-item--magazine').click(function () {
-    jQuery('.is-activated').removeClass('is-activated');
-    jQuery(this).find('.M__mainNavButton-main').addClass('is-activated');
-    jQuery('#__menuMagazine-main').addClass('is-activated');
-});
-
-
-jQuery('.O__mainNavList-item--magazine').find('.M__mainNavButton-main');
+    jQuery('.O__mainNavList-item--magazine').click(function () {
+        jQuery('.is-activated').removeClass('is-activated');
+        jQuery(this).find('.M__mainNavButton-main').addClass('is-activated');
+        jQuery('#__menuMagazine-main').addClass('is-activated');
+    });
 
 
-$(document).ready(function () {
-    $(".fancybox").fancybox({
+     jQuery('#__issueContent-col1-row2-row1-row1--signedOut').click(function () {
+        jQuery(this).addClass('is-activated');
+    });
+
+
+
+
+
+    jQuery('.O__mainNavList-item--magazine').find('.M__mainNavButton-main');
+    jQuery(".fancybox").fancybox({
         openEffect: 'fade',
-        /* none, fade, elastic */
         closeEffect: 'fade',
         openSpeed: 0,
-        /* ms, "slow", "normal", "fast"*/
         closeSpeed: 0,
-        /* mouseWheel : false,*/
         helpers: {
-            /*title : null */
             title: {
-                type: 'inside' /* 'float', 'inside', 'outside', 'over', 'null' */
+                type: 'inside'
             }
         }
     });
 });
-
